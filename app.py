dcf6e04ccb9f9fdc324ba9121795228459d43342
CorentinLeGuen <leguen.corentin@protonmail.com>
2023-01-19 15:30:18 -0500

    Create GRID system and first display

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from pyray import *
-from raylib import FLAG_WINDOW_UNDECORATED, FLAG_VSYNC_HINT, KEY_DOWN, KEY_UP, KEY_ENTER, KEY_LEFT, KEY_RIGHT
+from raylib import FLAG_WINDOW_UNDECORATED, FLAG_VSYNC_HINT, KEY_DOWN, KEY_UP, KEY_ENTER, KEY_LEFT, KEY_RIGHT, MOUSE_BUTTON_LEFT
+
+from grid import Grid
 
 
 class Application:
@@ -10,13 +12,31 @@ class Application:
         set_window_state(FLAG_VSYNC_HINT)
         set_target_fps(60)
 
+        self.grid_size = (10, 10)
+        self.grid_ratio = (800 // self.grid_size[0], 800 // self.grid_size[1])
+        self.grid = Grid(self.grid_size[0], self.grid_size[1])
+
     def __run__(self):
         while not window_should_close():
             begin_drawing()
             clear_background(RAYWHITE)
 
+            for row in range(self.grid_size[0]):
+                for col in range(self.grid_size[1]):
+                    draw_rectangle_lines(self.grid_ratio[0] * row, self.grid_ratio[1] * col, self.grid_ratio[0], self.grid_ratio[1], LIGHTGRAY)
+                    if self.grid.get_cell_at_position(row, col).is_alive:
+                        draw_rectangle(self.grid_ratio[0] * row, self.grid_ratio[1] * col, self.grid_ratio[0], self.grid_ratio[1], BLACK)
+                    else:
+                        draw_text(str(self.grid.get_cell_at_position(row, col).get_neighbors()), self.grid_ratio[0] * row + 10, self.grid_ratio[1] * col + 10, 18, LIGHTGRAY)
+
             draw_rectangle_lines(0, 0, 800, 800, DARKGRAY)
             draw_rectangle_lines(0, 799, 800, 101, DARKGRAY)
+
+            if is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+                mp = get_mouse_position()
+                x = int(mp.x // self.grid_ratio[0])
+                y = int(mp.y // self.grid_ratio[1])
+                self.grid.switch_at_position(x, y)
 
             end_drawing()
 
---
+++ grid.py
@@ -0,0 +1,85 @@
+class Cell:
+    def __init__(self):
+        self.is_alive = False
+        self.neighbors = 0
+
+    def add_neighbor(self):
+        self.neighbors += 1
+
+    def remove_neighbor(self):
+        self.neighbors -= 1
+
+    def get_neighbors(self) -> int:
+        return self.neighbors
+
+    def is_alive(self) -> bool:
+        return self.is_alive
+
+    def switch_alive(self):
+        if self.is_alive:
+            self.is_alive = False
+        else:
+            self.is_alive = True
+
+    def __repr__(self):
+        if self.is_alive:
+            return '(O, ' + str(self.get_neighbors()) + ')'
+        return '(X, ' + str(self.get_neighbors()) + ')'
+
+
+class Grid:
+    def __init__(self, x: int, y: int):
+        self.width = x
+        self.height = y
+        self.grid = [list([Cell() for _ in range(y)]) for _ in range(x)]
+
+    def tick(self):
+        # Apply 4 rules
+        return self.height * [self.width * [Cell()]]
+
+    def get_cell_at_position(self, x: int, y: int):
+        return self.grid[x][y]
+
+    def switch_at_position(self, x: int, y: int):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.grid[x][y].switch_alive()
+            if self.grid[x][y].is_alive:
+                if 0 <= x - 1:  # LEFT
+                    self.grid[x - 1][y].add_neighbor()
+                if 0 <= y - 1:  # UP
+                    self.grid[x][y - 1].add_neighbor()
+                    if 0 <= x - 1:  # UP / LEFT
+                        self.grid[x - 1][y - 1].add_neighbor()
+                    if x + 1 < self.width:  # UP / RIGHT
+                        self.grid[x + 1][y - 1].add_neighbor()
+                if x + 1 < self.width:  # RIGHT
+                    self.grid[x + 1][y].add_neighbor()
+                if y + 1 < self.height:  # DOWN
+                    self.grid[x][y + 1].add_neighbor()
+                    if 0 <= x - 1:  # LEFT
+                        self.grid[x - 1][y + 1].add_neighbor()  # DOWN / LEFT
+                    if x + 1 < self.width:  # RIGHT
+                        self.grid[x + 1][y + 1].add_neighbor()  # DOWN / RIGHT
+            else:
+                if 0 <= x - 1:  # LEFT
+                    self.grid[x - 1][y].remove_neighbor()
+                if 0 <= y - 1:  # UP
+                    self.grid[x][y - 1].remove_neighbor()
+                    if 0 <= x - 1:  # UP / LEFT
+                        self.grid[x - 1][y - 1].remove_neighbor()
+                    if x + 1 < self.width:  # UP / RIGHT
+                        self.grid[x + 1][y - 1].remove_neighbor()
+                if x + 1 < self.width:  # RIGHT
+                    self.grid[x + 1][y].remove_neighbor()
+                if y + 1 < self.height:  # DOWN
+                    self.grid[x][y + 1].remove_neighbor()
+                    if 0 <= x - 1:  # LEFT
+                        self.grid[x - 1][y + 1].remove_neighbor()  # DOWN / LEFT
+                    if x + 1 < self.width:  # RIGHT
+                        self.grid[x + 1][y + 1].remove_neighbor()  # DOWN / RIGHT
+
+    def __str__(self):
+        res = ''
+        for x in self.grid:
+            res += str(x) + ',\n'
+        return res
